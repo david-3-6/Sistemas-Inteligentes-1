0a0ae2cb8ca49849e66ac83113c86ab714934a90
David Cuesta Martín <david-3-6@users.noreply.github.com>
2023-03-14 10:26:23 +0100

    nuevaVersion

--- 1-Laberinto/src/laberinto/A.java
+++ 1-Laberinto/src/laberinto/A.java
@@ -1,27 +1,33 @@
 package laberinto;
 import java.util.ArrayList;
-import laberinto.*;
+import java.util.Arrays;
+
 public class A {
 
-    private int[] Inicio= new int[2];
-    private int[] Fin = new int[2];
-    private ArrayList<Nodo> closerSet;
-    private ArrayList<Nodo> openSet;
     private LabAzar laberinto;
+    private ArrayList<Nodo> closedSet;
+    private ArrayList<Nodo> openSet;
 
 
     public A (LabAzar laber){
-        Inicio=laber.Inicial;
-        Fin=laber.Fin;
-        laberinto=laber;
+    	laberinto=laber;
+        openSet.add(new Nodo(laber.Inicial[0],laber.Inicial[1],laber.Fin[0],laber.Fin[1]));
     }
-
-
-
-
     
+    public void camino () {
+    	Nodo nodoActual=new Nodo(openSet.get(0));
+    	int iter=0;
+    	while((openSet.size()>0) && (nodoActual.getX()!=laberinto.Fin[0] && nodoActual.getY()!=laberinto.Fin[1])) {
+    		nuevosNodos(openSet.get(0),laberinto);
+    	}
+
+    }
+    private void nuevosNodos (Nodo n, LabAzar laber) {
+    	int X=n.getX();
+    	int Y=n.getY();
+    	for(int i=0; i<4;i++) {
+    		
+    	}
+    }
     
-     
-
-
 }
--- 1-Laberinto/src/laberinto/Nodo.java
+++ 1-Laberinto/src/laberinto/Nodo.java
@@ -1,14 +1,32 @@
 package laberinto;
-import java.lang.Math;
+
 public class Nodo {
     private int CorX;
     private int CorY;
-    private int Dist;
+    private int Dist; //Realmente es el peso del nodo
 
     public Nodo(int X1, int Y1, int X2, int Y2){
         CorX = X1;
         CorY = Y1;
         Dist= Math.abs(X1-X2)+Math.abs(Y1-Y2);
+    }
+    public Nodo (Nodo n) {
+    	CorX=n.getX();
+    	CorY=n.getY();
+    	Dist=n.getDist();
+    	
+    }
+    public int getX() {
+    	return CorX;
+    }
+    public int getY() {
+    	return CorY;
+    }
+    public int getDist() {
+    	return Dist;
+    }
+    public void modDist(int n) {
+    	Dist+=n;
     }
 
 
